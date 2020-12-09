c803e4f6ecdb83a14e44b769eab74812592f5fce
simofleanta <simo.fleanta@gmail.com>
2020-12-09 19:29:02 +0100

    save

--- energy.py
+++ energy.py
@@ -3,10 +3,23 @@
 # This is an electric energy consumption analysis for threee regions, Berlin, GronIngen and Amsterdam.  
 #the data is taken from an people living in dofferent households, that are using this app 
 #we are interested to understand the Kwt consumption behaviour not only in different regions but aso in different households, for years 2018-2019
+#Anlyzing the Kwt behaviour accross the units, regions and its seasonality, may help us better understand how we waste energy or we save energy and therefore 
+#optimize costs and even be more evironmentally conscous. 
 
-#
+#the analysis wil take part in two stages
+#-part I - Active Kwt behaviour 
+#-part II - economic startup context 
 
 
+"""Part I"""
+
+#we did:
+#eda
+#pivotations 
+#correlations
+#pairplots that helped us understand the correlations
+#2d histogram to follow ditribution hourly kwt consumptipn accorss day, month or region
+#extracted the data according to region, month or number of room household and performed further analysis 
 
 
 #you want to it to show the distrib of your app accross the regions and benchmark with another similar app 
