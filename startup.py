440dd74e3fc5b0b2688bfb4c7f318e254257b222
simofleanta <simo.fleanta@gmail.com>
2020-12-09 21:18:44 +0100

    s

--- startup.py
+++ startup.py
@@ -20,6 +20,7 @@ from sklearn.metrics import r2_score
 import plotly.express as px
 
 #Business ASPECT
+#currency-euro
 
 #open file the energy file
 startup=pd.read_csv('energy_startup.csv')
@@ -27,9 +28,26 @@ print(startup.columns)
 df=DataFrame(startup.head(300))
 print(df.head(300))
 
-#Aggregate
+#I Aggregate revs by house hold rooms
+
 operations=['mean','sum','min','max']
 sales_household=df.groupby(['Month','Client_Room_household'], as_index=False)[['Sales_rev']].agg(operations)
 print(sales_household.reset_index())
+
+#After aggregation, values could be better seen in a bar chart 
+fig, ax=plt.subplots(figsize=(6,4))
+sns.set_style('darkgrid')
+df.groupby(['Client_Room_household','Month'])['Sales_rev'].sum().sort_values().plot(kind='bar')
+plt.ylabel('Sales_rev')
+ax.get_yaxis().get_major_formatter().set_scientific(False)
+plt.title('Sum of sale revenues according to household unit throughout months')
+plt.show()
+#it seems that the more rooms a houldhold has, the higher the sales revs.
+#summer autumn seem to have higher scores.
+
+
+
+
+
 
 
