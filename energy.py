7b6fe77826111a0bcce04d9a07bf39f468867404
simofleanta <simo.fleanta@gmail.com>
2020-12-09 12:04:46 +0100

    save

---
+++ energy.py
@@ -0,0 +1,26 @@
+#you have an app that monitors the electricity - kwtdata
+#you want it to show you how much you save or not 
+#you want to it to show the distrib of your app accross the regions and benchmark with another similar app 
+
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import plotly.express as px
+
+c=pd.read_csv('energy.csv')
+print(c.columns)
+df=DataFrame(c.head(500))
+print(df.head(500))
