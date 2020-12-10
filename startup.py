71d7b45f5ad5a5b607059cbc23686571d8fb094d
simofleanta <simo.fleanta@gmail.com>
2020-12-10 10:55:04 +0100

    update roi in the document

--- startup.py
+++ startup.py
@@ -186,17 +186,45 @@ fig = px.density_heatmap(Aarhus, x="weekday", y="Sales_rev", nbinsx=30, nbinsy=2
 #After seeing correlations and the max, means values pivots, we can now see the ROI on different months or seasons, keeping in mind that sales 
 #sale revenues increase autumn winter. 
 
-#2019 ROI
-#filtering the year
-
+#reopen file
 startup=pd.read_csv('energy_startup.csv')
 print(startup.columns)
 df=DataFrame(startup.head(700))
 print(df.head(700))
 
-year2019=df[df.Year==2019]
-print(year2019)
+#2019 ROI
+#filtering the year
 
+investment=100000
+year2019=df[df.Year==2019]
+
+costs=Costs=year2019['Costs']
+loss=Loss=year2019['Loss']
+
+#netprofit calculation
+net_profit19=costs*12-loss
+
+def generateROI(investment,costs,loss):
+    """function generating ROI for 2019"""
+    return net_profit19/investment*100
+print(generateROI(investment,costs,loss))
+
+#2018 ROI
+#filtering the year
+
+investment=70000
+year2018=df[df.Year==2018]
+
+#netprofit calculation
+net_profit18=costs*12-loss
+
+Costs=Costs=year2018['Costs']
+Loss=Loss=year2018['Loss']
+
+def generateROI(investment,Costs,Loss):
+    """function generating ROI for 2018"""
+    return net_profit18/investment*100
+print(generateROI(investment,Costs,Loss))
 
 
 
