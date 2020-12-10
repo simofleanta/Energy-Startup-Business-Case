cb96df4a99c3d7028fd6a229f22c09b0c367c272
simofleanta <simo.fleanta@gmail.com>
2020-12-10 10:52:28 +0100

    save roi

--- roi_energy.py
+++ roi_energy.py
@@ -27,15 +27,39 @@ print(startup.columns)
 df=DataFrame(startup.head(700))
 print(df.head(700))
 
-
 #2019 ROI
 #filtering the year
+
+investment=100000
 year2019=df[df.Year==2019]
 
 costs=Costs=year2019['Costs']
 loss=Loss=year2019['Loss']
 
 #netprofit calculation
-net_profit=costs*12-loss
+net_profit19=costs*12-loss
+
+def generateROI(investment,costs,loss):
+    """function generating ROI for 2019"""
+    return net_profit19/investment*100
+print(generateROI(investment,costs,loss))
+
+#2018 ROI
+#filtering the year
+
+investment=100000
+year2018=df[df.Year==2018]
+
+#netprofit calculation
+net_profit18=costs*12-loss
+
+Costs=Costs=year2018['Costs']
+Loss=Loss=year2018['Loss']
+
+def generateROI(investment,Costs,Loss):
+    """function generating ROI for 2018"""
+    return net_profit18/investment*100
+print(generateROI(investment,Costs,Loss))
+
 
 
