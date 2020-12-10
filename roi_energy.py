52c7c36c66c059801a5afb20f24234f5fdfd9a44
simofleanta <simo.fleanta@gmail.com>
2020-12-10 10:33:24 +0100

    add net_prof calculation for roi

--- roi_energy.py
+++ roi_energy.py
@@ -19,13 +19,23 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
 import plotly.express as px
 
-#2019 ROI
-#filtering the year
 
+
+#open file
 startup=pd.read_csv('energy_startup.csv')
 print(startup.columns)
 df=DataFrame(startup.head(700))
 print(df.head(700))
 
+
+#2019 ROI
+#filtering the year
 year2019=df[df.Year==2019]
-print(year2019)
+
+costs=Costs=year2019['Costs']
+loss=Loss=year2019['Loss']
+
+#netprofit calculation
+net_profit=costs*12-loss
+
+
