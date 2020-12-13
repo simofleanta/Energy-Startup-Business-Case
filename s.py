472601c67f613cc6b8300f0fec7e44c86ddc5772
simofleanta <simo.fleanta@gmail.com>
2020-12-13 12:50:03 +0100

    s

--- s.py
+++ s.py
@@ -38,19 +38,20 @@ print(sdf.head(700))
 
 #taking info from both datasets into one df. #merge 
 
-startup_df=sdf[['Year','Month','Sales_rev','Season']].copy()
+startup_df=sdf[['Year','Month','Sales_rev','Season','Month_Profit','Client_Region','Client_Room_household']].copy()
 endf=df[['Year','Month','Active_kwh_month','Kwh']].copy()
 z_merge=pd.merge(startup_df,endf)
 print(z_merge)
 
-#2. interest kwh season
-#last valuefor kwh per season
-last_kwh_season=z_merge[z_merge.Kwh>0].groupby('Season', as_index=False).last()[['Kwh','Sales_rev']]
-print(last_kwh_season)
 
-#First valuefor kwh per season
-first_month_season=z_merge[z_merge.Kwh>0].groupby('Season', as_index=False).first()[['Kwh','Season','Year','Month','Sales_rev']]
-print(first_month_season)
+#x=sns.relplot('Sales_rev','Month_Profit',data=z_merge,hue='Season', size='Year')
+#plt.show()
+
+December=z_merge[z_merge.Month=='Dec']
+sp=sns.stripplot(December.Sales_rev, December.Client_Region)
+plt.show()
+
+
 
 
 
